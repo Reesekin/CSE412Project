5ba6834cee5929ea3e7183362c9ef9d089779e0a
Reesekin <terret343@gmail.com>
2022-04-20 02:18:30 -0700

    added more details to pie/donut chart

--- charts.js
+++ charts.js
@@ -1,3 +1,11 @@
+var selectedArc;
+
+async function arcSelect(arc){
+    await d3.selectAll("use").attr("xlink:href", `#rlabel${arc}`);
+    selectedArc = d3.select(`#rlabel${arc}`);
+    console.log(selectedArc);
+}
+
 async function drawPieChart(){
     var data = rsum;
     var tots = d3.sum(data, function(d){return d.surfacearea;});
@@ -52,7 +60,7 @@ async function drawPieChart(){
         .attr("fill", function(d, i) {
             return color(i);
         })
-        .attr("onmouseover", function(d, i) {return `(function(){d3.selectAll("use").attr("xlink:href", "#rlabel${i}")})()`})
+        .attr("onmouseover", function(d, i) {return `arcSelect(${i})`;})
         .attr("d", arc);
 
     arcs.append("text")
@@ -61,58 +69,23 @@ async function drawPieChart(){
         .attr("id", function(d, i) {return "rlabel" + i;})
         .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";  })
         .text(function(d) {
-            return `${d.data.rname}\n${d.data.percentage}%`;
+            return `${d.data.rname}`;
         });
+    arcs.selectAll("text")
+    .append("tspan")
+    .attr("x", "0")
+    .attr("dy", "1.2em")
+    .text(function(d) {
+        return `${d.data.percentage}%`;
+    })
+    arcs.selectAll("text")
+    .append("tspan")
+    .attr("x", "0")
+    .attr("dy", "1.2em")
+    .text(function(d) {
+        return `${d.data.surfacearea} km²`;
+    })
     d3.select(".piechart")
     .append("use")
     .attr("xlink:href", "0");
-}
-
-async function drawBar(){
-    var data = dataset;
-    var svg = d3.select("#population"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        margin = {top: 20, right: 20, bottom: 30, left: 40},
-        g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var x = d3.scaleBand()
-        .rangeRound([0, width])
-        .padding(0.1);
-
-    var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
-
-    var xAxis = d3.axisBottom(x);
-
-    var yAxis = d3.axisLeft(y)
-        .ticks(10);
-
-    x.domain(data.map(function(d) { return d.country; }));
-    y.domain([0, d3.max(data, function(d) { return d.population; })]);
-
-    g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    g.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Population");
-
-    g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.country); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.population); })
-        .attr("height", function(d) { return height - y(d.population); });
 }
