56f083fdcee6330d9ca4676cc8c69bb5edb4f1d7
Reesekin <terret343@gmail.com>
2022-04-20 23:17:09 -0700

    theme adjustments

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const pool = new Pool({
   user: "postgres",
   database: "countryprofiles",
   password: "root",
-  port: 8888
+  port: 5432
 });
-
-
 
 //SELECT json_agg(t) FROM (SELECT * FROM countries NATURAL JOIN regions NATURAL JOIN population NATURAL JOIN economy ORDER BY countrykey ASC) t;
 async function getData(){
